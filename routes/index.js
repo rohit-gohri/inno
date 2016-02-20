459e66270cef0eb382d94d91560b42a16f390caf
Rohit Gohri <gohri.rohit@gmail.com>
2016-02-20 13:17:26 +0530

    fixed passport fb scope and updated error page

--- routes/index.js
+++ routes/index.js
@@ -42,7 +42,7 @@ router.post('/register', function (req, res) {
     });
 });
 
-router.get('/login/fb', passport.authenticate('facebook', {authType: 'rerequest', scope: 'email'}));
+router.get('/login/fb', passport.authenticate('facebook', {authType: 'rerequest', scope: ['email']}));
 
 router.get('/login/fb/callback',
     passport.authenticate('facebook', {
