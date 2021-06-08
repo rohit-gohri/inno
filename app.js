f3ec65710a342978c942f54a780fc723e696a799
rohit <rohitvmaurya847@gmail.com>
2021-06-08 00:22:25 -0700

    updated jade to pug

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var hashids = new Hashids(config.get('hashids').secret, config.get('hashids').no
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
